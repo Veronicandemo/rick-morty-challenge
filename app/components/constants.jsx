ed89eb414e9390943ccfa50999f04c42505e9f96
Veronicandemo <ndemoveronica@gmail.com>
2024-04-16 16:26:16 +0300

    initial commit

---
+++ app/components/constants.jsx
@@ -0,0 +1,21 @@
+import { Icon } from "@iconify/react";
+
+export const SIDEBAR_ITEMS = [
+  {
+    title: "Characters",
+    path: "/",
+    icon: <Icon icon="lucide:home" width="24" height="24" />,
+  },
+  {
+    title: "Location",
+    path: "/location",
+    icon: <Icon icon="lucide:folder" width="24" height="24" />,
+
+  },
+  {
+    title: "Episodes",
+    path: "/episodes",
+    icon: <Icon icon="lucide:mail" width="24" height="24" />,
+  },
+ 
+];
---
+++ app/components/search.jsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import React from 'react'
+import { IoSearch } from "react-icons/io5";
+
+
+const Search = () => {
+    return (
+      <>
+      <div className='flex flex-col rounded-md w-16 h-10 items-center justify-center bg-[#01AFCA] text-white'>
+          <IoSearch width={36} height={36} />
+          
+            </div>
+          
+      </>
+  )
+}
+
+export default Search
